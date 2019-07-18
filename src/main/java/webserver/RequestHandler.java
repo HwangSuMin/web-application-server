63fd09cdff54c543f772cd0405caa38d85e85158
황수민 <sumin974@outlook.com>
2019-07-19 01:09:23 +0900

    3.4.3.1 요구사항 1 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -19,13 +18,31 @@ public class RequestHandler extends Thread {
     }
 
     public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            // index.html 응답 1단계
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String line = br.readLine();
+            log.debug("request line : {}", line);
+
+            if (line == null) {
+                return;
+            }
+
+            String[] tokens = line.split(" ");
+
+            while (!line.equals("")) {
+                line = br.readLine();
+                log.debug("header : {}", line);
+            }
+
+            File requestFile = new File("./webapp" + tokens[1]);
+            byte[] body = Files.readAllBytes(requestFile.toPath());
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello Sumin World\n".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {

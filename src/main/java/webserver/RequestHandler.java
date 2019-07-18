e74cccec1673a67477710a2ab0cc720455f9c609
황수민 <sumin974@outlook.com>
2019-07-19 01:11:35 +0900

    3.4.3.2 요구사항 2 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,12 +1,15 @@
 package webserver;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -33,18 +36,26 @@ public class RequestHandler extends Thread {
             }
 
             String[] tokens = line.split(" ");
+            String url = tokens[1];
 
             while (!line.equals("")) {
                 line = br.readLine();
                 log.debug("header : {}", line);
             }
 
-            File requestFile = new File("./webapp" + tokens[1]);
-            byte[] body = Files.readAllBytes(requestFile.toPath());
-
-            DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            if (url.startsWith("/user/create")) {
+                String queryString = url.substring(url.indexOf("?") + 1);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("User : {}", user);
+            }
+            else {
+                DataOutputStream dos = new DataOutputStream(out);
+                File requestFile = new File("./webapp" + url);
+                byte[] body = Files.readAllBytes(requestFile.toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
